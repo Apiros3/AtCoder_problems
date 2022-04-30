8af20fe690210183e42dcadd1220f808ca1c7846
Apiros3 <100747246+Apiros3@users.noreply.github.com>
2022-04-30 13:19:33 +0900

    Add files via upload

---
+++ ABC/053/053A_AC.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+
+int main()
+{
+    int X;
+    cin >> X;
+
+    if (X<1200) 
+        cout << "ABC";
+    else
+        cout << "ARC";
+
+    return 0;
+}     
